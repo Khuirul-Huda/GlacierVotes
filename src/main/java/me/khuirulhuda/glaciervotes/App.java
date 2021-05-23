a172c7617ed88966d210f5e80350bd6d9b04888e
Khuirul-Huda <hudah2102@gmail.com>
2021-05-23 13:03:01 +0700

    initial commit

---
+++ src/main/java/me/khuirulhuda/glaciervotes/App.java
@@ -0,0 +1,13 @@
+package me.khuirulhuda.glaciervotes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
