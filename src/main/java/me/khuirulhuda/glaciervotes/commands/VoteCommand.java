80b67f277fd7dea83551caba17b95e5c23e696be
Khuirul-Huda <hudah2102@gmail.com>
2021-09-30 00:21:17 +0700

    init VoteCommand.java

--- src/main/java/me/khuirulhuda/glaciervotes/commands/VoteCommand.java
+++ src/main/java/me/khuirulhuda/glaciervotes/commands/VoteCommand.java
@@ -1,1 +1,17 @@
-//TO DO
+package me.khuirulhuda.glaciervotes.commands;
+
+import me.khuirulhuda.glaciervotes.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class VoteCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        return true;
+    }
+}
