40ffae94a174db7c11d4a91793d8cc4c163b0e3b
Khuirul-Huda <hudah2102@gmail.com>
2022-07-02 13:15:03 +0700

    fix initialize & format

--- src/main/java/me/khuirulhuda/glaciervotes/events/Vote.java
+++ src/main/java/me/khuirulhuda/glaciervotes/events/Vote.java
@@ -20,184 +20,184 @@ import me.khuirulhuda.glaciervotes.Main;
 
 public class Vote implements Listener {
 
-    @EventHandler(priority=EventPriority.HIGHEST)
-    public void onPlayerJoin(PlayerJoinEvent event) {
-      
-      final boolean shouldNotice = Main.getInstance().getConfig().getBoolean("notice");
-      final String nvmsg = Main.getInstance().getConfig().getString("nvmessage");
-      final boolean debugmode = Main.getInstance().getConfig().getBoolean("debug");
-       if (debugmode) {
-         debug("Player Joined");
-       }
-       
-     final String apikey = Main.getInstance().getConfig().getString("apikey");
-      
-      if (debugmode) {
-        debug("Using API KEY : "+apikey);
-      }
-      
-      final Player player = event.getPlayer();
-      String name = player.getName();
-      if (debugmode) {
-        debug("Player Joined "+name);
-      }
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerJoin(PlayerJoinEvent event) {
 
-    boolean spasi = name.contains(" ");
-     String fname = name;
-     String gname = name;
-    
-    if (spasi) {
-      int sploc = gname.indexOf(" ");
-      gname = gname.substring(0, sploc);
-    } else {
-    }
-    
-if (spasi) {
-   fname = name.replace(" ", "%20");
-  //String api = "https://minecraftpocket-servers.com/api/?object=votes&element=claim&key="+apikey+"&username="+fname;
-} 
-      //final String httperrormsg = "Internal Error has occured! Try Again Later";
-      final String api = "https://minecraftpocket-servers.com/api/?object=votes&element=claim&key="+apikey+"&username="+fname;
-      final List<String> listcmd = Main.getInstance().getConfig().getStringList("commands");
-      final String gnamee = gname;
-      final String fnamee = fname;
-      //Webhook Variables
-      final String whservername = Main.getInstance().getConfig().getString("servername");
-      final String whfooter = Main.getInstance().getConfig().getString("footer");
-      final String whthumbnail = Main.getInstance().getConfig().getString("thumbnail");
-      final String whusername = Main.getInstance().getConfig().getString("username");
-      final String whdescription = Main.getInstance().getConfig().getString("description").replaceAll("%player%", gnamee);
-      final String whavatar = Main.getInstance().getConfig().getString("avatarurl");
-      final String whurl = Main.getInstance().getConfig().getString("webhook");
-      final boolean whenabled = !whurl.equalsIgnoreCase("null");
-      
-Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> {
-    
-try {
-URL url = new URL(api);
-HttpURLConnection http = (HttpURLConnection)url.openConnection();//start 
+		final boolean shouldNotice = Main.getInstance().getConfig().getBoolean("notice");
+		final String nvmsg = Main.getInstance().getConfig().getString("nvmessage");
+		final boolean debugmode = Main.getInstance().getConfig().getBoolean("debug");
+		if (debugmode) {
+			debug("Player Joined");
+		}
 
-int status = http.getResponseCode();
-String httpresponse = http.getResponseMessage();//end
+		final String apikey = Main.getInstance().getConfig().getString("apikey");
 
-if (debugmode) {
-  debug("HTTP Response Code:"+status);
-  debug("HTTP Response:"+httpresponse);
-}
+		if (debugmode) {
+			debug("Using API KEY : " + apikey);
+		}
 
-int responseCode = http.getResponseCode();
-    InputStream inputStream;
-    if (200 <= responseCode && responseCode <= 299) {
-        inputStream = http.getInputStream();
-    } else {
-        inputStream = http.getErrorStream();
-    }
+		final Player player = event.getPlayer();
+		String name = player.getName();
+		if (debugmode) {
+			debug("Player Joined " + name);
+		}
 
-    BufferedReader in = new BufferedReader(
-        new InputStreamReader(
-            inputStream));
+		boolean spasi = name.contains(" ");
+		String fname = name;
+		String gname = name;
 
-    StringBuilder respons = new StringBuilder();
-    String currentLine;
-    while ((currentLine = in.readLine()) != null) 
-        respons.append(currentLine);
-    in.close();
-    
-    String response = respons.toString();
+		if (spasi) {
+			int sploc = gname.indexOf(" ");
+			gname = gname.substring(0, sploc);
+		} else {
+		}
 
-    if (debugmode){
-      debug("Response: "+response);
-    }
+		if (spasi) {
+			fname = name.replace(" ", "%20");
+			// String api =
+			// "https://minecraftpocket-servers.com/api/?object=votes&element=claim&key="+apikey+"&username="+fname;
+		}
+		// final String httperrormsg = "Internal Error has occured! Try Again Later";
+		final String api = "https://minecraftpocket-servers.com/api/?object=votes&element=claim&key=" + apikey
+				+ "&username=" + fname;
+		final List<String> listcmd = Main.getInstance().getConfig().getStringList("commands");
+		final String gnamee = gname;
+		final String fnamee = fname;
+		// Webhook Variables
+		final String whservername = Main.getInstance().getConfig().getString("servername");
+		final String whfooter = Main.getInstance().getConfig().getString("footer");
+		final String whthumbnail = Main.getInstance().getConfig().getString("thumbnail");
+		final String whusername = Main.getInstance().getConfig().getString("username");
+		final String whdescription = Main.getInstance().getConfig().getString("description").replaceAll("%player%",
+				gnamee);
+		final String whavatar = Main.getInstance().getConfig().getString("avatarurl");
+		final String whurl = Main.getInstance().getConfig().getString("webhook");
+		final boolean whenabled = !whurl.equalsIgnoreCase("null");
 
-if ( 200 <= responseCode && responseCode <= 299 ) {
-  if ( response.contains("1")) {
-    
-    for (String command : listcmd) {
-      
-      if (spasi) {
-        Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+		Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> {
 
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("%player%", gnamee));
-        });
-      } else {
-        Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+			try {
+				URL url = new URL(api);
+				HttpURLConnection http = (HttpURLConnection) url.openConnection();// start
 
-    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("%player%", player.getName()));
-        });
-      }
-}
-    //player.sendMessage("Terimakasih sudah vote");
-    String claimapiurl = "http://minecraftpocket-servers.com/api/?action=post&object=votes&element=claim&key="+apikey+"&username="+fnamee;
- 
-  try {
-  URL urll = new URL(claimapiurl);
-HttpURLConnection httpp = (HttpURLConnection)urll.openConnection();//start
+				int status = http.getResponseCode();
+				String httpresponse = http.getResponseMessage();// end
 
-int statuss = httpp.getResponseCode();
-String responsee = httpp.getResponseMessage();//end
-if (debugmode){
-  debug("POST REQUEST CODE:"+statuss);
-  debug("POST RESPONSE:"+responsee);
-} 
-httpp.disconnect();
-} catch (IOException p) {
-  Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+				if (debugmode) {
+					debug("HTTP Response Code:" + status);
+					debug("HTTP Response:" + httpresponse);
+				}
 
-  String logp = p.toString();
-  Main.getInstance().getLogger().severe(logp);
-  });
-}
-    //Webhook
-    if (whenabled) {
-            DiscordWebhook webhook = new DiscordWebhook(whurl);
-      webhook.setContent("GlacierVotes Beta V2");
-      webhook.setAvatarUrl(whavatar);
-      webhook.setUsername(whusername);
-      webhook.setTts(true);
-      webhook.addEmbed(new DiscordWebhook.EmbedObject()
-            .setTitle(whservername)
-            .setDescription(whdescription)
-            .setColor(Color.GREEN)
-            .addField("Logging", "true", true)
-             .setAuthor("GlacierVotes Beta V2", "https://github.com/Khuirul-Huda", "https://avatars.githubusercontent.com/u/67778682?s=48&v=4")
-    .setThumbnail(whthumbnail)
-    .setFooter(whthumbnail, whfooter)
-    );
-      webhook.execute();
-      
-    }
-  } 
-  if ( response.contains("2")) {
-    //voted claimed
-  }
-  if ( response.contains("0")) {
-    // not found || no votes
-    if (shouldNotice) {
-    player.sendMessage(ChatColor.YELLOW+nvmsg);
-    }
-  }
-} else {
-  Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+				int responseCode = http.getResponseCode();
+				InputStream inputStream;
+				if (200 <= responseCode && responseCode <= 299) {
+					inputStream = http.getInputStream();
+				} else {
+					inputStream = http.getErrorStream();
+				}
 
-  String logme = "Error"+status+response;
-  Main.getInstance().getLogger().severe(logme);
-  });
-}
-http.disconnect();
-} catch(IOException q){
-  Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+				BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
 
-  String qstr = q.toString();
-  Main.getInstance().getLogger().severe(qstr);
-  });
-}
-    });//async?
-    }
+				StringBuilder respons = new StringBuilder();
+				String currentLine;
+				while ((currentLine = in.readLine()) != null)
+					respons.append(currentLine);
+				in.close();
 
-public void debug(String debugstr) {
-  Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+				String response = respons.toString();
 
-  Main.getInstance().getLogger().warning(ChatColor.WHITE+debugstr);
-  });
-}
+				if (debugmode) {
+					debug("Response: " + response);
+				}
+
+				if (200 <= responseCode && responseCode <= 299) {
+					if (response.contains("1")) {
+
+						for (String command : listcmd) {
+
+							if (spasi) {
+								Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+
+									Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+											command.replace("%player%", gnamee));
+								});
+							} else {
+								Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+
+									Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+											command.replace("%player%", player.getName()));
+								});
+							}
+						}
+						String claimapiurl = "http://minecraftpocket-servers.com/api/?action=post&object=votes&element=claim&key="
+								+ apikey + "&username=" + fnamee;
+
+						try {
+							URL urll = new URL(claimapiurl);
+							HttpURLConnection httpp = (HttpURLConnection) urll.openConnection();// start
+
+							int statuss = httpp.getResponseCode();
+							String responsee = httpp.getResponseMessage();// end
+							if (debugmode) {
+								debug("POST REQUEST CODE:" + statuss);
+								debug("POST RESPONSE:" + responsee);
+							}
+							httpp.disconnect();
+						} catch (IOException p) {
+							Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+
+								String logp = p.toString();
+								Main.getInstance().getLogger().severe(logp);
+							});
+						}
+						// Webhook
+						if (whenabled) {
+							DiscordWebhook webhook = new DiscordWebhook(whurl);
+							webhook.setContent("GlacierVotes Beta V2");
+							webhook.setAvatarUrl(whavatar);
+							webhook.setUsername(whusername);
+							webhook.setTts(true);
+							webhook.addEmbed(new DiscordWebhook.EmbedObject().setTitle(whservername)
+									.setDescription(whdescription).setColor(Color.GREEN)
+									.addField("Logging", "true", true)
+									.setAuthor("GlacierVotes Beta V2", "https://github.com/Khuirul-Huda",
+											"https://avatars.githubusercontent.com/u/67778682?s=48&v=4")
+									.setThumbnail(whthumbnail).setFooter(whthumbnail, whfooter));
+							webhook.execute();
+
+						}
+					}
+					if (response.contains("2")) {
+						// voted claimed
+					}
+					if (response.contains("0")) {
+						// not found || no votes
+						if (shouldNotice) {
+							player.sendMessage(ChatColor.YELLOW + nvmsg);
+						}
+					}
+				} else {
+					Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+
+						String logme = "Error" + status + response;
+						Main.getInstance().getLogger().severe(logme);
+					});
+				}
+				http.disconnect();
+			} catch (IOException q) {
+				Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+
+					String qstr = q.toString();
+					Main.getInstance().getLogger().severe(qstr);
+				});
+			}
+		});// async?
+	}
+
+	public void debug(String debugstr) {
+		Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+			if (Main.getInstance().getConfig().getBoolean("debug"))
+				Main.getInstance().getLogger().warning(ChatColor.WHITE + debugstr);
+		});
+	}
 }
